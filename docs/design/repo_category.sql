4e28cbd9cfd40ed81accc12caac8b6bff29621b1
dengxz <6882855@qq.com>
2019-03-11 20:32:12 +0800

    t

---
+++ docs/design/repo_category.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: select * from repo_category
+LIMIT 0, 500
+
+-- Date: 2019-03-11 20:31
+*/
+INSERT INTO `repo_category` (`cat_id`,`parent_id`,`cat_name`,`cat_prio`,`cat_status`) VALUES (1,0,'总体',0,1);
+INSERT INTO `repo_category` (`cat_id`,`parent_id`,`cat_name`,`cat_prio`,`cat_status`) VALUES (2,0,'产品',1,1);
+INSERT INTO `repo_category` (`cat_id`,`parent_id`,`cat_name`,`cat_prio`,`cat_status`) VALUES (3,0,'技术',2,1);
+INSERT INTO `repo_category` (`cat_id`,`parent_id`,`cat_name`,`cat_prio`,`cat_status`) VALUES (4,0,'经营',3,1);
+INSERT INTO `repo_category` (`cat_id`,`parent_id`,`cat_name`,`cat_prio`,`cat_status`) VALUES (5,0,'对手',4,1);
