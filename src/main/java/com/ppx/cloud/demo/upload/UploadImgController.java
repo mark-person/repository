e644fea7ebc01aa39be5efaa725981ceb0188466
dengxz <6882855@qq.com>
2019-03-20 16:28:42 +0800

    t

--- src/main/java/com/ppx/cloud/demo/upload/UploadImgController.java
+++ src/main/java/com/ppx/cloud/demo/upload/UploadImgController.java
@@ -30,31 +30,29 @@ import com.ppx.cloud.common.util.ApplicationUtils;
 @Controller
 public class UploadImgController {
 	
-	private final static String UPLOAD = "img/";
+	private final static String IMG_UPLOAD_PATH = "img/";
 	
-	private final static String IDEA_MODULE = "idea/";
-	
-	private final static String MAIN = "main/";
-	
-	private final static String ADDITIONAL = "additional/";
+	private final static String KNOWLEDGE_MODULE = "knowledge/";
+	private final static String KNOWLEDGE_MAIN = "main/";
+	private final static String KNOWLEDGE_ADDITIONAL = "additional/";
 	
 	
-	public Map<?, ?> uploadIdea(@RequestParam("file") MultipartFile[] files) throws Exception {
+	public Map<?, ?> uploadKnowledge(@RequestParam("file") MultipartFile[] files) throws Exception {
 		
 	
 		
 		var returnList = new ArrayList<String>();
 		
 		// 不存就创建文件夹 >>>>>>>>>>>>>>>>>>>>>
-		String modulePath = ApplicationUtils.JAR_PARENT_HOME + UPLOAD + IDEA_MODULE;
-		// 10天一个文件夹
+		String modulePath = ApplicationUtils.JAR_PARENT_HOME + IMG_UPLOAD_PATH + KNOWLEDGE_MODULE;
+		// 7天一个文件夹2019w11
 		Date today = new Date();
 		// String.format("%tj", d)一年的第几天
-		String dateFolder = String.format("%ty", today)
+		String dateFolder = String.format("%tY", today) + "w"
 				+ String.format("%02d", Integer.parseInt(String.format("%tj", today)) / 10) + "/";
 					
-		String mainPath = modulePath + dateFolder + MAIN;
-		String additionalPath = modulePath + dateFolder + ADDITIONAL;
+		String mainPath = modulePath + dateFolder + KNOWLEDGE_MAIN;
+		String additionalPath = modulePath + dateFolder + KNOWLEDGE_ADDITIONAL;
 		File mainPathFile = new File(mainPath);
 		if (!mainPathFile.exists()) {
 			mainPathFile.mkdirs();
@@ -75,27 +73,25 @@ public class UploadImgController {
 				file.transferTo(new File(mainPath + imgFileName));
 				
 				// 缩放
-				try {
-					// convert -resize 200x100 src.jpg dest.jpg 200×100(等比缩放)
-					String miniPath = mainPath + imgFileName + "_100.png";
-					String command = "convert -resize 100x100 " + mainPath + imgFileName + " " + miniPath;
-					Process process = Runtime.getRuntime().exec(command);
-					InputStream inputStream = process.getErrorStream();
-					String cmdResult = new BufferedReader(new InputStreamReader(inputStream, "GBK")).lines()
+				// convert -resize 200x100 src.jpg dest.jpg 200×100(等比缩放)
+				String miniPath = mainPath + imgFileName + "_100.png";
+				String command = "convert -resize 100x100 " + mainPath + imgFileName + " " + miniPath;
+				Process process = Runtime.getRuntime().exec(command);
+				try (InputStream inputStream = process.getErrorStream();) {
+					String cmdResult = new BufferedReader(new InputStreamReader(inputStream, "UTF-8")).lines()
 							.collect(Collectors.joining(System.lineSeparator()));
-					inputStream.close();
 					if (!StringUtils.isEmpty(cmdResult)) {
 						return ReturnMap.of(4001, "convert执行结果出错:" +cmdResult);
 					}
 				} catch (Exception e) {
 					return ReturnMap.of(4002, "转换命令出错:" + e.getMessage());
 				}
-				returnList.add(IDEA_MODULE + dateFolder + MAIN + imgFileName);
+				returnList.add(KNOWLEDGE_MODULE + dateFolder + KNOWLEDGE_MAIN + imgFileName);
 			}
 			else {
 				// >>>>>>>>>>>>>>>>>附加
 				file.transferTo(new File(additionalPath + imgFileName));
-				returnList.add(IDEA_MODULE + dateFolder + ADDITIONAL + imgFileName);
+				returnList.add(KNOWLEDGE_MODULE + dateFolder + KNOWLEDGE_ADDITIONAL + imgFileName);
 			}
 		}
 		
