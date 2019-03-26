ecf297333be3a4c1bbce3ca34e0361bec1982ca1
dengxz <6882855@qq.com>
2019-03-26 18:27:50 +0800

    t

---
+++ src/main/java/com/ppx/cloud/repository/mobile/HomeController.java
@@ -0,0 +1,19 @@
+package com.ppx.cloud.repository.mobile;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+	
+	
+	public ModelAndView mIndex(ModelAndView mv) {
+		
+		
+		
+		return mv;
+	}
+	
+
+	
+}
