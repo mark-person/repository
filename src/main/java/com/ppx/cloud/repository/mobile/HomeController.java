15d65853a5b82498df6a23b15504f5581c65b572
mark <13602493280@139.com>
2019-03-28 22:24:52 +0800

    t

--- src/main/java/com/ppx/cloud/repository/mobile/HomeController.java
+++ src/main/java/com/ppx/cloud/repository/mobile/HomeController.java
@@ -1,14 +1,35 @@
 package com.ppx.cloud.repository.mobile;
 
+import java.util.Date;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.ppx.cloud.auth.common.AuthContext;
+import com.ppx.cloud.auth.common.LoginAccount;
+import com.ppx.cloud.auth.config.AuthUtils;
+import com.ppx.cloud.common.util.CookieUtils;
 
 @Controller
 public class HomeController {
 	
 	
-	public ModelAndView mIndex(ModelAndView mv) {
+	public ModelAndView mIndex(ModelAndView mv, HttpServletResponse response) {
 		
+		LoginAccount account = AuthContext.getLoginAccount();
+		if (account == null) {
+			// modified用来校验帐号或密码被修改
+			Algorithm algorithm = Algorithm.HMAC256(AuthUtils.getJwtPassword());
+			String token = JWT.create().withIssuedAt(new Date()).withClaim("accountId", -1)
+					.withClaim("loginAccount", "admin").withClaim("userId", -1)
+					.withClaim("userName", "admin").withClaim("modified", new Date().getTime())
+					.sign(algorithm);
+			CookieUtils.setCookie(response, AuthUtils.PPXTOKEN, token);
+		}
 		
 		
 		return mv;
