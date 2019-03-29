bd29cb53c3d5a475499c7d63112825fff8c80974
dengxz <6882855@qq.com>
2019-03-29 10:21:18 +0800

    t

--- src/main/java/com/ppx/cloud/repository/mobile/HomeController.java
+++ src/main/java/com/ppx/cloud/repository/mobile/HomeController.java
@@ -1,7 +1,10 @@
 package com.ppx.cloud.repository.mobile;
 
+import java.io.IOException;
 import java.util.Date;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
@@ -10,8 +13,6 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
-import com.ppx.cloud.auth.common.AuthContext;
-import com.ppx.cloud.auth.common.LoginAccount;
 import com.ppx.cloud.auth.config.AuthUtils;
 import com.ppx.cloud.common.util.CookieUtils;
 
@@ -20,12 +21,13 @@ public class HomeController {
 	
 	
 	@GetMapping("/index")
-	public ModelAndView mIndex(ModelAndView mv, HttpServletResponse response) {
+	public ModelAndView mIndex(ModelAndView mv, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
 		
 		mv.setViewName("repository/mobile/home/mIndex");
 		
-		LoginAccount account = AuthContext.getLoginAccount();
-		if (account == null) {
+		//LoginAccount account = AuthContext.getLoginAccount();
+		//if (account == null) {
 			// modified用来校验帐号或密码被修改
 			Algorithm algorithm = Algorithm.HMAC256(AuthUtils.getJwtPassword());
 			String token = JWT.create().withIssuedAt(new Date()).withClaim("accountId", -1)
@@ -33,9 +35,19 @@ public class HomeController {
 					.withClaim("userName", "admin").withClaim("modified", new Date().getTime())
 					.sign(algorithm);
 			CookieUtils.setCookie(response, AuthUtils.PPXTOKEN, token);
-		}
-		
-		
+			
+			//response.addHeader(name, value);
+		//}
+			//response.setHeader("Refresh", "0;URL=mIndex");
+			response.setHeader("Content-Type", "text/html; charset=utf-8");
+		request.getRequestDispatcher("mIndex").include(request, response);
+		//	response.sendRedirect("mIndex");
+		return null;
+	}
+	@GetMapping("/mIndex")
+	public ModelAndView mIndex(ModelAndView mv) throws ServletException, IOException {
+		mv.setViewName("repository/mobile/home/mIndex");
+		System.out.println("----");
 		return mv;
 	}
 	
