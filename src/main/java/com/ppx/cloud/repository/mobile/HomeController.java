af5a639964451385b3f47603d4377dba6c0bf3a4
dengxz <6882855@qq.com>
2019-04-02 15:31:54 +0800

    t

--- src/main/java/com/ppx/cloud/repository/mobile/HomeController.java
+++ src/main/java/com/ppx/cloud/repository/mobile/HomeController.java
@@ -1,7 +1,6 @@
 package com.ppx.cloud.repository.mobile;
 
 import java.io.IOException;
-import java.util.Base64;
 import java.util.Date;
 
 import javax.servlet.ServletException;
@@ -10,7 +9,6 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.auth0.jwt.JWT;
@@ -24,38 +22,16 @@ public class HomeController {
 	
 	@GetMapping("/m")
 	public ModelAndView mIndex(ModelAndView mv, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// /需要转码，不能用来做文件名
-		// mysql   FULLTEXT
-//		try {
-//			String b = Base64.getEncoder().encodeToString(",a我是中国人b".getBytes());
-//			System.out.println("sssssss1:" + b);
-//			byte[] bb = Base64.getDecoder().decode("LGHmiJHmmK/kuK3lm73kurpi");
-//			System.out.println("sssssss2:" + new String(bb));
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
+
+		Algorithm algorithm = Algorithm.HMAC256(AuthUtils.getJwtPassword());
+		String token = JWT.create().withIssuedAt(new Date()).withClaim("accountId", -1)
+				.withClaim("loginAccount", "admin").withClaim("userId", -1)
+				.withClaim("userName", "admin").withClaim("modified", new Date().getTime())
+				.sign(algorithm);
+		CookieUtils.setCookie(response, AuthUtils.PPXTOKEN, token);
 		
-		
-		
-		
-		//mv.setViewName("repository/mobile/home/mIndex");
-		
-		//LoginAccount account = AuthContext.getLoginAccount();
-		//if (account == null) {
-			// modified用来校验帐号或密码被修改
-			Algorithm algorithm = Algorithm.HMAC256(AuthUtils.getJwtPassword());
-			String token = JWT.create().withIssuedAt(new Date()).withClaim("accountId", -1)
-					.withClaim("loginAccount", "admin").withClaim("userId", -1)
-					.withClaim("userName", "admin").withClaim("modified", new Date().getTime())
-					.sign(algorithm);
-			CookieUtils.setCookie(response, AuthUtils.PPXTOKEN, token);
-			
-			//response.addHeader(name, value);
-		//}
-			//response.setHeader("Refresh", "0;URL=mIndex");
-			response.setHeader("Content-Type", "text/html; charset=utf-8");
-		request.getRequestDispatcher("auto/knowledge/mListKnowledge").include(request, response);
-		//	response.sendRedirect("mIndex");
+		response.setHeader("Content-Type", "text/html; charset=utf-8");
+		request.getRequestDispatcher("auto/mobile/home").include(request, response);
 		return null;
 	}
 	
---
+++ src/main/java/com/ppx/cloud/repository/mobile/MobileController.java
@@ -0,0 +1,110 @@
+package com.ppx.cloud.repository.mobile;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.logging.log4j.util.Strings;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.ppx.cloud.common.contoller.ReturnMap;
+import com.ppx.cloud.common.page.MPage;
+import com.ppx.cloud.repository.category.KnowledgeCategoryService;
+import com.ppx.cloud.repository.knowledge.Knowledge;
+import com.ppx.cloud.repository.knowledge.KnowledgeServiceImpl;
+import com.ppx.cloud.repository.usp.KnowledgeUspService;
+
+@Controller
+public class MobileController {
+	
+	@Autowired
+	private KnowledgeServiceImpl impl;
+	
+    private final static String INIT_CONTENT = "# \n* \n* ";
+   
+    @Autowired
+    private KnowledgeCategoryService categoryService;
+    @Autowired
+    private KnowledgeUspService uspService;
+    
+    public ModelAndView home(ModelAndView mv) {
+		mv.addObject("list", list(new MPage(), new Knowledge()));
+		mv.addObject("catList", categoryService.list());
+		return mv;
+	}
+    
+    public Map<?, ?> list(MPage page, Knowledge pojo) {
+		return ReturnMap.of(page, impl.mList(page, pojo));
+	}
+    
+    public ModelAndView add(ModelAndView mv) {
+    	mv.setViewName("repository/mobile/mobile/know");
+    	
+    	Knowledge pojo = new Knowledge();
+    	pojo.setkContent(INIT_CONTENT);
+    	mv.addObject("pojo", pojo);
+		mv.addObject("catList", categoryService.list());
+		mv.addObject("uspList", uspService.list());
+		
+		// init_recommend
+		mv.addObject("starList", (List<String>)List.of("", "", ""));
+		mv.addObject("noStarList", (List<String>)List.of("", ""));		
+		return mv;
+	}
+    
+    public ModelAndView edit(ModelAndView mv, @RequestParam Integer id) {
+    	mv.setViewName("repository/mobile/mobile/know");
+    	Knowledge pojo = impl.get(id);
+    	if (Strings.isEmpty(pojo.getkContent())) {
+    		pojo.setkContent(INIT_CONTENT);
+    	}
+		mv.addObject("pojo", pojo);
+		mv.addObject("catList", categoryService.list());
+		
+		if (Strings.isNotEmpty(pojo.getImgSrc())) {
+			mv.addObject("imgList", pojo.getImgSrc().split(","));
+    	}
+		
+		// init_recommend
+		List<String> starList = new ArrayList<String>();
+		for (int i = 0; i < pojo.getRecommend(); i++) {
+			starList.add("");
+		}
+		mv.addObject("starList", starList);
+		List<String> noStarList = new ArrayList<String>();
+		for (int i = 0; i < 5 - pojo.getRecommend(); i++) {
+			noStarList.add("");
+		}
+		mv.addObject("noStarList", noStarList);
+		return mv;
+	}
+    
+    public Map<?, ?> insertOrUpdate(Knowledge pojo) {
+    	if (pojo.getkId() == null) {
+    		return impl.insert(pojo);
+    	}
+    	else {
+    		return impl.update(pojo);
+    	}
+    }
+    
+    public ModelAndView view(ModelAndView mv, @RequestParam Integer id) {
+    	Knowledge pojo = impl.get(id);
+		mv.addObject("pojo", pojo);
+		
+		if (Strings.isNotEmpty(pojo.getImgSrc())) {
+			mv.addObject("imgList", pojo.getImgSrc().split(","));
+    	}
+		
+		// init_recommend
+		List<String> starList = new ArrayList<String>();
+		for (int i = 0; i < pojo.getRecommend(); i++) {
+			starList.add("");
+		}
+		mv.addObject("starList", starList);
+		return mv;
+	}
+}
